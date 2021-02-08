3e1e11926b7b983ae6728be22cdf205ce3123bd5
ABHIJEET JAIN <66820627+lucifer0311@users.noreply.github.com>
2021-02-08 09:27:20 +0530

    Add files via upload

---
+++ tut5_For_While_do-while loops.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+// FOR LOOP //
+    
+    /* for(int i=0 ; i<6 ; i++){
+        cout<<i<<endl;
+        i++;
+    }   */ 
+
+// WHILE LOOP //
+    
+   /* int i=0;
+    while(i<=40){
+        cout<<i<<endl;
+        i++;
+    }    */ 
+    
+// DO-WHILE //
+
+    int i=0;
+    do{
+        cout<<i<<endl;    /* iss case me loop ek baar toh run karega hi karega */
+        i++;
+    }while(false);
+
+    return 0;
+ 
+}
