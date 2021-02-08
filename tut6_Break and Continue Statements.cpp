7eeade3ab63c7ff88c327ae9cab4d4da739cd9ec
ABHIJEET JAIN <66820627+lucifer0311@users.noreply.github.com>
2021-02-08 10:05:53 +0530

    Add files via upload

---
+++ tut6_Break and Continue Statements.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+// BREAK STATEMENTS //
+
+ /*   for(int i=0 ; i<5 ; i++){
+       
+        if(i==3){
+            break;
+        }
+        cout<<i<<endl;
+    }     */
+
+// CONTINUE STATEMENTS // 
+
+     for(int i=0 ; i<10 ; i++){
+         if(i==1){
+             continue;
+         }
+         cout<<i<<endl;
+     }    
+    return 0;
+
+}
